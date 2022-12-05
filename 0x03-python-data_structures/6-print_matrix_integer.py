df068e4cf5757d479c55889a66b3181724761de0
dacoolguy1 <emmanueltemiede@gmail.com>
2022-12-05 02:08:13 -0800

    Deleted the main.c details here

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,12 +5,3 @@ def print_matrix_integer(matrix=[[]]):
             print("{:d}".format(val), end=" " if val != row[-1] else "")
         print()
 
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
