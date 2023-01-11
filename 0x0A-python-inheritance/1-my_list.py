896a361a07d4b21c8d074d6825a1489c034b9a21
dacoolguy1 <emmanueltemiede@gmail.com>
2023-01-11 01:06:01 -0800

    first

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+Class that inherits from list
+"""
+
+
+class Mylist(list):
+    """
+    Args:
+        list: class list
+    """
+
+    def print_sorted(self):
+        """Print the lists in a sorted ascending manner"""
+
+        cpy_list = self.copy()
+        cpy_list.sort()
+        print("{}".format(cpy_list))
