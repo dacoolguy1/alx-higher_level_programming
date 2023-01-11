a7889941bf92ad51657d1000ea8a2310005e089c
Emmanuel Temiede <68757523+dacoolguy1@users.noreply.github.com>
2023-01-11 09:45:08 +0100

    updated

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,17 +1,16 @@
-#!/usr/bin/python3
-"""
-Module 0-lookup
-Function that returns the list of available attributes
-and methods of an object
-"""
-
-
-def lookup(obj):
-    """
-    Args:
-        obj: instance of the class
-    Returns:
-        List of attributes
-    """
-
-    return dir(obj)
+#!/usr/bin/python3                                                                                                                                       
+"""                                                                                                                                                      
+Module 0-lookup                                                                                                                                          
+We are tp write a                                                                                                                                        
+function that returns the list of available attributes and methods of an object                                                                          
+"""                                                                                                                                                      
+                                                                                                                                                         
+                                                                                                                                                         
+def lookup(obj):                                                                                                                                         
+    """                                                                                                                                                  
+    Args:                                                                                                                                                
+        obj: instance of the class                                                                                                              
+    Returns:                                                                                                                                             
+        List of attributes                                                                                                                               
+    """                                                                                                                                                  
+    return dir(obj)        
