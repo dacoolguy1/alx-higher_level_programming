380f7c3ec6b86ec031e03cf19e000b8f785aff08
dacoolguy1 <emmanueltemiede@gmail.com>
2023-01-11 10:47:28 -0800

    23

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+public instance method that raises an exception
+"""
+
+
+class BaseGeometry:
+    """
+    This is an empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
