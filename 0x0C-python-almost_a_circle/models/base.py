121df54f8aabb66c46e55978ee39f2f2fce114f3
dacoolguy1 <emmanueltemiede@gmail.com>
2023-01-18 01:12:16 -0800

    corrected styling error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,13 +6,15 @@ id with this argument value
 otherwise, increment __nb_objects and assign the new value
 to the public instance attribute id
 """
+
+
 class Base:
     """
     Main class
     """
-    __nb_objects = 0 
+    __nb_objects = 0
     """ a private atttribute hence it ca only be acceed inside this class"""
-    
+
     def __init__(self, id=None):
         """ initializes objects attributes"""
         if id is not None:
