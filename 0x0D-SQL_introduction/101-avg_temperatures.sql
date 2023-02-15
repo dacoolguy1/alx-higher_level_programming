cf1bfa65122573f590572ac3ab8b01c9309e04ba
Emmanuel Temiede <68757523+dacoolguy1@users.noreply.github.com>
2023-02-15 17:44:22 +0100

    top city

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,8 +1,6 @@
 -- mysql -u username -p  databasename  < path/example.sql to import tables to database
--- Write a script that displays the average temperature (Fahrenheit) by city
+-- Write a script that displays the average temperature (Fahrenheit) by city.
 -- ordered by temperature (descending)
 SELECT city, AVG(value) as avg_temp FROM temperatures
-WHERE month = 7 OR month = 8
 GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+ORDER BY avg_temp DESC;
