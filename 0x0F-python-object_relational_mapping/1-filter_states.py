a37005e05f4f4744b6b795e746db80dd68876098
Emmanuel Temiede <68757523+dacoolguy1@users.noreply.github.com>
2023-03-29 09:23:16 +0100

    TEsting

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa
-the script takes 3 arguments
+""""lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
 if __name__ == "__main__":
     import MySQLdb
@@ -11,9 +11,11 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        charset='utf8')
+        charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa
-the script takes 3 arguments
+"""takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections
 """
 if __name__ == "__main__":
     import MySQLdb
@@ -11,9 +12,13 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        charset='utf8')
+        charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute(
+        """SELECT * FROM states WHERE \
+name = %s ORDER BY id ASC""", (sys.argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
+    cur.close()
+    conn.close()
